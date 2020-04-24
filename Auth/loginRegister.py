fed9ee57916253046b6bc7f0ba70e9af70160f19
Daniel Lovett <51034848+danlovett@users.noreply.github.com>
2020-04-24 14:18:48 +0100

    Changelog Friday 24th April @ 1300z
    Things improved:
    - Functions
        > Less functions. Bunched code together where similar concepts were being achieved (directRegister & redirectReason)
        > Organised better.
    - Wording
        > Wording makes more sense
        > Less repitition
        > Better user feedback. Less confusion occurring: 'restart to save changes'. 'Changes saved under username [userName]'
        > Better user feedback in userName and password creation. (how many characters in the password, comparing the variable with requirements)
    
    Things Fixed:
    - More efficiant code. Removed temporary fixes, with proper solutions (not work arounds).
    - Wording {Mentioned in 'Things Improved'}
    - Infinite loops which weren't recognised in earlier commits.
    - Fixed how getting number of lines from a file is achieved. Removed the Function, using better variable names in the different functions.
    
    What's New:
    - New file explaining different redirect codes/meanings (Location: 'Misc/dataRAW.md')
    - Comments to better understand the organisation of the code.
    
    To continue testing to find errors/bugs.
    Start work on Game (Location: 'App/...').
    ** Using code from eariler development as base. Can be found in Dan Lovett's One Drive: https://1drv.ms/u/s!AqHdygAtlbyk2F7bgU7C9eozP50i?e=vZwkD9 **

--- Auth/loginRegister.py
+++ Auth/loginRegister.py
@@ -1,38 +1,122 @@
-#Importing time and linecache from modules
-import time
+### START OF LOGIN/REGISTER ###
+
+#Importing linecache from modules ('time' module later for code delays)
 import linecache
 
 # Setting the variables
 catchLineLogin = 1
-
+welcomeRetry = 0
 userName = False
 password = False
 redirect = False
-welcomeRetry = 0
 
-def directRegister(userName, redirect):
-    print("The Account with the username '" + userName + "' was not found\n--> Registration")
-    registration(userName, redirect)
+### FUNCTIONS ###
 
-def redirectReason(redirect):
+#REDIRECT (User Feedback)
+def redirectReason(userName, redirect):
     if redirect == 'data_load_missing':
-        print("You was redirected as the data from the login file was missing/corrupt.\nYou must register to continue")
+        print("Data missing from File.\n--> Registration")
+        registration(userName, redirect)
     elif redirect == 'username_load_error':
         print("--> Registration")
+        registration(userName, redirect)
     elif redirect == 'from_missing_reload':
-        print("You was redirected as the data from the login file was missing/corrupt.\nYou must restart to continue")
+        print("Data has been saved in the file, under the username '" + userName + "'. You must restart to continue")
+        input("Press ENTER")
         exit()
     else:
         pass
 
-def login(userName, password, userNameCheck, passwordCheck, passwordFailCounter = 0, userFailCounter = 0, firstTime = True):
+#Registration
+def registration(userName, redirect):
 
-    catchLineLogin = 1
     count = 0
 
     with open('Auth/Inc/Login.txt', 'r') as f:
         for line in f:
             count += 1
+
+    userAccept = False
+    login = False
+
+    firstName = input("What is your first name?\nFirst Name: ").capitalize()
+    lastName = input("What is your last name?\nLast Name: ").capitalize()
+    
+    while userAccept == False:
+
+        userName = input("Your username must have:\n  - At least 4 characters\n  - No spaces\nUsername: ").lower()
+
+        if len(userName) < 4:
+            print("'" + userName + "' was not long enough\nTry again")
+            continue
+        elif ' ' in userName:
+            print("'" + userName + "' contains a space.\nTry again")
+            continue
+        elif ' ' in userName and len(userName) < 4:
+            print("'" + userName + "' was not long enough and a space was present\nTry again")
+            continue
+        else:
+            catchLineLogin = 1
+            login = False
+
+            fileInfo = linecache.getline('Auth/Inc/Login.txt', catchLineLogin)
+            dataBlock = fileInfo.split(',')
+
+            if (dataBlock[0] == userName and count == catchLineLogin) or (userName == dataBlock[0]):
+                print("Username already taken, try a different one")
+                userAccept = False
+
+            elif (dataBlock [0] != userName and count == catchLineLogin) or (count == 0):
+                print("Username Accepted")
+                userAccept = True
+
+            else:
+                catchLineLogin = catchLineLogin + 1
+                continue
+
+    print("Your password must have:\n  - At least 8 (eight) characters\n  - Contain AT LEAST 1 upper character")
+
+    while login == False:
+        password = input("Password: ")
+
+        if len(password) < 8:
+            print("-->The Password was not long enough. (" + str(len(password)) + " characters)\nTry again")
+            continue
+        elif ' ' in password:
+            print("-->The Password contains a space\nTry again")
+            continue
+        elif not any(x.isupper() for x in password):
+            print("-->The password contains no upper cases\nTry again")
+            continue
+        elif not any(x.isdigit() for x in password):
+            print("-->The password contains no numbers\nTry again")
+            continue
+        else:
+            print("Success!")
+            writingLogin = open('Auth/Inc/Login.txt', 'a')
+            writingLogin.write(userName + "," + password + "," + firstName + "," + lastName + ",account\n")
+            writingLogin.close()
+            break
+
+    if redirect == 'username_load_error' or redirect == 'data_load_missing':
+        redirectReason(userName, redirect = 'from_missing_reload')
+
+#Login
+def login(userName, password, userNameCheck, passwordCheck, passwordFailCounter = 0, userFailCounter = 0, firstTime = True):
+
+    catchLineLogin = 1
+    count = 0
+
+    try:
+        with open('Auth/Inc/Login.txt', 'r') as f:
+            for line in f:
+                count += 1
+        if count == 0:
+            redirectReason(userName, redirect = 'data_load_missing')
+    except IOError:
+        file = open('Auth/Inc/Login.txt', 'w+')
+        file.close()
+        redirectReason(userName, redirect= 'from_missing_reload')
 
     infoFile = linecache.getline('Auth/Inc/Login.txt', catchLineLogin)
     dataBlock = infoFile.split(',')
@@ -53,7 +137,7 @@ def login(userName, password, userNameCheck, passwordCheck, passwordFailCounter 
                     print(userName + " user not found. You have " + str(userAttempsRemains) + " attempts remaining before being directed to the register page")
                 
                 elif userFailCounter == 3:
-                    directRegister(userName, redirect = "username_load_error")
+                    redirectReason(userName, redirect = "username_load_error")
                     userNameCheck = True
         
         else:
@@ -80,82 +164,10 @@ def login(userName, password, userNameCheck, passwordCheck, passwordFailCounter 
             if passwordFailCounter == 3:
                 print("You have failed to log in.\nRestart the program and try again")
                 exit()
-    
-#REGISTRATION
-def registration(userName, redirect):
-
-    count = 0
-
-    with open('Auth/Inc/Login.txt', 'r') as f:
-        for line in f:
-            count += 1
-
-    userAccept = False
-    login = False
-
-    firstName = input("What is your first name?\nFirst Name: ").capitalize()
-    lastName = input("What is your last name?\nLast Name: ").capitalize()
-    
-    while userAccept == False:
-        userName = input("Your username must have:\n  - At least 4 characters\n  - No spaces\nUsername: ").lower()
-        if len(userName) < 4:
-            print("Error\nThe username was not long enough\nTry again")
-            continue
-        elif ' ' in userName:
-            print("Error\nThe username contains a space.\nTry again")
-            continue
-        elif ' ' in userName and len(userName) < 4:
-            print("Error\nThe username was not long enough and a space was present\nTry again")
-            continue
-        else:
-            catchLineLogin = 1
-            login = False
-
-            fileInfo = linecache.getline('Auth/Inc/Login.txt', catchLineLogin)
-            dataBlock = fileInfo.split(',')
-
-            if (dataBlock[0] == userName and count == catchLineLogin) or (userName == dataBlock[0]):
-                print("Username already taken, try a different one")
-                userAccept = False
-
-            elif (dataBlock [0] != userName and count == catchLineLogin) or (count == 0):
-                print("Username Accepted")
-
-                userAccept = True
-
-            else:
-                catchLineLogin = catchLineLogin + 1
-                continue
-
-    while login == False:
-        password = input("Your password must have:\n  - At least 8 (eight) characters\n  - Contain AT LEAST 1 upper character\nPassword: ")
-
-        if len(password) < 8:
-            print("Error\nThe username was not long enough\nTry again")
-            continue
-        elif ' ' in password:
-            print("Error\nThe username contains (a) space[s]\nTry again")
-            continue
-        elif not any(x.isupper() for x in password):
-            print("Error\nThe password contains no upper cases\nTry again")
-            continue
-        elif not any(x.isdigit() for x in password):
-            print("Error\nThe password contains no numbers\nTry again")
-            continue
-        else:
-            print("Success!")
-            break
-
-    writingLogin = open('Auth/Inc/Login.txt', 'a')
-    writingLogin.write(userName + "," + password + "," + firstName + "," + lastName + ",account\n")
-    writingLogin.close()
-
-    if redirect == 'username_load_error' or redirect == 'data_load_missing':
-        redirectReason(redirect = 'from_missing_reload')
 
 ### END OF FUNCTIONS ###
 
-### ACCESSING FUNCTIONS ###
+### ACCESSING FUNCTIONS/PROGRAM ###
 while True:
     welcome = input("Login/Register (Type Either 'Register' or 'Login'): ").capitalize()
     if welcome == "Login":
@@ -171,3 +183,7 @@ while True:
         print("Error\nTry again")
         welcomeRetry = welcomeRetry + 1
         continue
+
+### END OF ACCESSING FUNCTIONS ###
+
+### END OF LOGIN/REGISTER ###
---
+++ Misc/dataRAW.md
@@ -0,0 +1,14 @@
+Login/Register Codes:
+
+data_load_missing
+  Reason: No data found in file. Missing
+  Error code: 404
+
+username_load_error
+  Reason: Username not found. 
+  Error code: 404
+
+from_missing_reload
+  Reason: New data in file. Restart to load new file
+  Error code = 404
+
