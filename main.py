4a752684cda99712cfa6418237769419c356a4c7
Daniel Lovett <51034848+danlovett@users.noreply.github.com>
2020-06-14 21:59:18 +0100

    Minor Changes
    Clearup of files before confirming changes to complete auth stage of program.
    
    Things Changed:
     - File reference naming (makes more sense now)
     - Delete unnecassary files (dataRAW contained error message references that no longer exist)
    
    User Auth can now be found under 'Auth/auth.py'

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import sys
 import time
 
 #Finding path of required filed for the game to run
-sys.path.insert(1, 'Auth/loginRegisterNEW')
+sys.path.insert(1, 'Auth/auth')
 sys.path.insert(1, 'App/game') #TO PUT SCORING SYS IN WITH GAME
 
 welcomeAnswer = input("Hello!\nReady to get started? (Y/N)\nY/N: ").capitalize()
 
 if welcomeAnswer == "Y":
-    from Auth import loginRegisterNEW
+    from Auth import auth
 elif welcomeAnswer == "N":
     exit("OK. See you in a bit.")
 else:
