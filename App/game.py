d7ece7251e62aab8b44eaa88ec771b9a8bd00373
Daniel Lovett <51034848+danlovett@users.noreply.github.com>
2020-06-24 01:18:01 +0100

    Coding Cluster Reduction. Wednesday 24 June @ 01:20
    # Bug Fixes and Improvements
    # Major improvements to code presentation.
     - Clustering code together reduces file sizes and improves efficiancy.
     - Some parts were not possible to compress/bring together due to syntax errors arising.

--- App/game.py
+++ App/game.py
@@ -1,37 +1,27 @@
-
 import linecache, random, time as t, os
 from datetime import datetime
-
-u_attempt = 0
-mGenreType = ''
-day = datetime.today().strftime('%Y-%m-%d')
+u_attempt, mGenreType, day = 0, '', datetime.today().strftime('%Y-%m-%d')
 
 class Game():
 
-    def logout(self):
-        os.remove('Auth/Inc/currentUser.txt')
+    def logout(self): os.remove('Auth/Inc/currentUser.txt')
 
     def totline(self, f_type):
         global line
         line = 1
-        with open(f'{f_type}.txt', 'r') as f: 
-            for i in f: line += 1 , i
+        with open(f'{f_type}.txt', 'r') as f:  
+            for i in f: line += 1
+            i
     
     def UIdata(self):
         global q, qrg
-        
         Game().sys_data(r_redir = 'options')
-
         q = input(f"Please select from the following options to begin playing: {sys_genre}\n").title()
-
         if q == sys_genre:
             Game().totline(f_type = f'App/Inc/{q}')
             qrg = list(range(1, line))
-            input(f'Okay, running {sys_genre} music now.\nPress ENTER to continue.'), t.sleep(0.5), os.system('cls' if os.name=='nt' else 'clear')
-            Music().UIdata()
-        else:
-            os.system('cls' if os.name=='nt' else 'clear')
-            print(f'"{q}" is not a music genre avaliable for this quiz.\nRestart the program and try again.')
+            input(f'Okay, running {sys_genre} music now.\nPress ENTER to continue.'), t.sleep(0.5), os.system('cls' if os.name=='nt' else 'clear'), Music().UIdata()
+        else: os.system('cls' if os.name=='nt' else 'clear'), print(f'"{q}" is not a music genre avaliable for this quiz.\nRestart the program and try again.')
         
     def sys_data(self, r_redir):
         global name, firstName, lastName, userName, sys_song, sys_artist, sys_releaseY, sys_fStrSong, sys_hint, sys_genre, rd_line, sys_highScore, sys_userName, sys_fullName
@@ -41,12 +31,10 @@ class Game():
             rd_line = 1
         elif r_redir == 'r_music':
             f_type = f'App/Inc/{q}'
-            rd_line = int(random.choice(qrg))
-            qrg.remove(rd_line)
+            rd_line = int(random.choice(qrg)), qrg.remove(rd_line)
         elif r_redir == 'options':
             f_type = 'App/Inc/.avaliOption'
             rd_line = 1
-
         elif r_redir == 'rh_scr':
             f_type = f'App/Score/{q}'
             rd_line = 1
@@ -56,99 +44,59 @@ class Game():
             dataBlock = contents.split('|')
 
             if r_redir == 'rc_u':
-                name = dataBlock[0]
-                userName = dataBlock[1]
+                name = dataBlock[0], userName = dataBlock[1]
                 nameSplit = name.split(' ')
-                firstName = nameSplit[0]
-                lastName = nameSplit[1]
-                sys_fullName = dataBlock[0]
-
-            elif r_redir == 'r_music':
-                sys_song = dataBlock[0]
-                sys_artist = dataBlock[1]
-                sys_releaseY = dataBlock[2]
-                sys_fStrSong = sys_song[0]
-                sys_hint = sys_song[1]
-
-            elif r_redir == 'options':
-                sys_genre = dataBlock[0] # to be changed, allowing compile of array to string and saving to new array list n.
-
-            elif r_redir == 'rh_scr':
-                sys_highScore = dataBlock[1]
-                sys_userName = dataBlock[2]
-
-                linecache.clearcache()
-        else:
-            print('User Authentification Failed.\nLogin First.\n\nError Code: AUTH_ERROR')
-            exit()
+                firstName = nameSplit[0], lastName = nameSplit[1], sys_fullName = dataBlock[0]
+            elif r_redir == 'r_music': sys_song = dataBlock[0], sys_artist = dataBlock[1], sys_releaseY = dataBlock[2], sys_fStrSong = sys_song[0], sys_hint = sys_song[1]
+            elif r_redir == 'options': sys_genre = dataBlock[0] # to be changed, allowing compile of array to string and saving to new array list n.
+            elif r_redir == 'rh_scr': sys_highScore = dataBlock[1], sys_userName = dataBlock[2], linecache.clearcache()
+        else: print('User Authentification Failed.\nLogin First.\n\nError Code: AUTH_ERROR'), exit()
     
     def attempt(self):
         global u_attempt
-
         u_attempt += 1
-
-        if u_attempt == 3:
-            print("Attempts exceeded. Try again later.")
-            exit()
+        if u_attempt == 3: print("Attempts exceeded. Try again later."), exit()
 
 class Music:
 
     def UIdata(self):
         global q_num
-        r_redir = False
-        song_c = False
-        q_num = 1
-        pa_y = 'yes'
-
+        bt, r_redir, song_c, q_num, pa_y = False, False, False, 1, 'yes'
         Game().sys_data(r_redir = 'rc_u')
-        
-        while True:
-            Game().sys_data(r_redir = 'r_music')
-            Game().totline(f_type = f'App/Inc/{q}')
-
-            if sys_song == None:
+        while bt == False:
+            Game().sys_data(r_redir = 'r_music'), Game().totline(f_type = f'App/Inc/{q}')
+            if sys_song == None: 
                 input(f'{firstName}, you have completed this set of questions.\nCurrent Score is {score}.\nPress ENTER to exit.')
-                break
-
+                bt = True
+                
             q_u = input(f'Question {q_num}\n{"-"*50}\nFirst Letter of Song: {sys_fStrSong} | Artist: {sys_artist}\n{"-"*20}\nSong Name: ').title()
-
             if q_u == sys_song:
                 Scoring().score(r_redir = 'song_c'), Scoring().highscore(r_redir, xvl = False)
-                r_redir = True
-                song_c = True
+                r_redir, song_c = True, True
+            elif q_u == 'Exit': bt = True, print(f'Thanks for playing {userName}!')
 
             if song_c == True:
                 qu = input(f'Do you know what year "{sys_song}" by "{sys_artist}" was released?\n{"-"*20}\nRelease Year: ')
-
-                if qu == str(sys_releaseY):
-                    Scoring().score(r_redir = 'ry_c'), Scoring().highscore(r_redir, xvl = False)
+                if qu == str(sys_releaseY): Scoring().score(r_redir = 'ry_c'), Scoring().highscore(r_redir, xvl = False)
 
             if qrg == []:
                 t.sleep(.5)
                 os.system('cls' if os.name=='nt' else 'clear')
-                while True:
+                btNest = False
+                while btNest == False:
                     pa_y = input(f'{firstName}, that is the end of the set! Want to play again? (yes/no)\n').lower()
+                    if pa_y == 'yes': btNest = True, Game().UIdata()
+                    elif pa_y == 'no': btNest = True, print(f'Thanks for playing!\nRemember your username "{userName}" for next time!')
+                    else: Game().attempt(), print('error'), t.sleep(.2), os.system('cls' if os.name=='nt' else 'clear')
 
-                    if pa_y == 'yes':
-                        Game().UIdata()
-                        break
-                    elif pa_y == 'no':
-                        print(f'Thanks for playing!\nRemember your username "{userName}" for next time!')
-                        break
-                    else:
-                        Game().attempt()
-                        print('error')
-                        t.sleep(.2), os.system('cls' if os.name=='nt' else 'clear')
-            if pa_y != 'no':
-                b = input('\nPress ENTER to continue. Type "exit" to leave the program\n').lower()
-                if b == 'exit':
-                    os.system('cls' if os.name=='nt' else 'clear')
-                    print(f'High Score update: {Scoring().highscore(r_redir = False, xvl = True)}')
-                    Scoring().score(r_redir = 'x_wrdsk')
-                    break
-            else:
-                break
-
+                    if pa_y != 'no':
+                        b = input('\nPress ENTER to continue. Type "exit" to leave the program\n').lower()
+                        if b == 'exit':
+                            os.system('cls' if os.name=='nt' else 'clear')
+                            print(f'High Score update:')
+                            Scoring().highscore(r_redir = False, xvl = True), Scoring().score(r_redir = 'x_wrdsk')
+                            bt = True
+                    else: btNest = True
             os.system('cls' if os.name=='nt' else 'clear')
             q_num += 1
         
@@ -157,61 +105,37 @@ class Scoring:
     def score(self, r_redir):
         Game().sys_data(r_redir = 'rc_u')
         global score
-        try:
-            score = 0
-        except NameError:
-            score = 0
-        if r_redir == 'song_c':
-            print('Song Correct') # change this wording
-            score += 1
-        elif r_redir == 'ry_c':
-            print('R Year Correct') # change this wording
-            score += 2
-        elif r_redir == 'g_scr':
-            return score
-
+        try: score = 0
+        except NameError: score = 0
+        if r_redir == 'song_c': score += 1, print('Song Correct') # change this wording
+        elif r_redir == 'ry_c': score += 2, print('R Year Correct') # change this wording
+        elif r_redir == 'g_scr': return score
         elif r_redir == 'x_wrdsk':
-            with open(f'App/Score/{q}-{userName}.txt', 'a') as f:
-                f.write(f'{day}|{score}|{userName}|{sys_fullName}\n')
-                f.close()
+            with open(f'App/Score/{q}-{userName}.txt', 'a') as f: f.write(f'{day}|{score}|{userName}|{sys_fullName}\n'), f.close()
         
     def highscore(self, r_redir, xvl):
         if r_redir == False:
             Game().sys_data(r_redir = 'rh_scr'), Game().sys_data(r_redir = 'rc_u'), Scoring().score(r_redir = 's_scr')
             if sys_userName == 'undefined' and sys_highScore == 'undefined':
                 print('Well done! You have just set the first high score for this genre!')
-                with open(f'App/Score/{q}.txt', 'w') as f:
-                    f.write(f'{day}|{score}|{userName}|{sys_fullName}\n')
-                    f.close()
+                with open(f'App/Score/{q}.txt', 'w') as f: f.write(f'{day}|{score}|{userName}|{sys_fullName}\n'), f.close()
                 t.sleep(4)
-            
             elif score > int(sys_highScore) and sys_userName == userName:
                 print("You just beat your own score! Well done!")
-                with open(f'App/Score/{q}.txt', 'w') as f:
-                    f.write(f'{day}|{score}|{userName}|{sys_fullName}')
-                    f.close()
+                with open(f'App/Score/{q}.txt', 'w') as f: f.write(f'{day}|{score}|{userName}|{sys_fullName}'), f.close()
                 t.sleep(2)
             elif xvl == True:
-                print('No highscore\'s beaten. Try again another day.')
-                
+                print('No highscore\'s beaten. Try again another day.') 
         elif score > int(sys_highScore) and sys_userName != userName:
                 print(f'Well done! you just over took {sys_userName} (AKA "{sys_fullName}") with your highscore: {score} (their\'s was {sys_highScore}).')
-                with open(f'App/Score/{q}.txt', 'w') as f:
-                    f.write(f'{day}|{score}|{userName}|{sys_fullName}')
-                    f.close()
+                with open(f'App/Score/{q}.txt', 'w') as f: f.write(f'{day}|{score}|{userName}|{sys_fullName}'), f.close()
                 t.sleep(4)
-
 
 Game().sys_data(r_redir = 'rc_u')
 if userName == 'danlovett':
     r = input('Reset data? (reset)\n').lower()
     if r == 'reset':
-        with open('App/Score/Pop.txt', 'w') as f:
-            f.write('undefined|'*4)
-            f.close()
+        with open('App/Score/Pop.txt', 'w') as f: f.write('undefined|'*4), f.close()
         exit('Complete.')
-
-Game().UIdata()
-Game().logout()
-
+Game().UIdata(), Game().logout()
 exit(f'Goodbye, {firstName}')
--- Auth/auth.py
+++ Auth/auth.py
@@ -1,24 +1,14 @@
-### FOR FUTURE: use userID for better auth ###
-
-import linecache as l, time, os, sys as s
+import time, os, sys as s, linecache as l
 s.path.insert(1, 'App/game')
-
-u_attempt = 0
-c_line = 1
-
-rfdbk = ''
-u_rfdbk = ''
+u_attempt, c_line = 0, 1
+rfdbk, u_rfdbk = '', ''
 
 class General:
-    # Handling Getting file contents | computing userAttempts made
-
     def sys_data(self, r_action):
         global sys_name, sys_userName, sys_password, c_line
         
-        if r_action == '+ln':
-            c_line += 1
-        elif r_action == '.ln':
-            c_line = 1
+        if r_action == '+ln': c_line += 1
+        else: c_line = 1
 
         if os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK):
             data = l.getline('Auth/Inc/login.txt', c_line)
@@ -26,129 +16,86 @@ class General:
             sys_name = dataBlock[0]
             sys_userName = dataBlock[1]
             sys_password = dataBlock[2]
-
             l.clearcache()
 
     def c_user(self, name, userName):
-        with open('Auth/Inc/currentUser.txt', 'w') as f:
-            f.write(f'{name}|{userName}|')
-            f.close()
-        
+        with open('Auth/Inc/currentUser.txt', 'w') as f: f.write(f'{name}|{userName}|'), f.close()  
         print('Enjoy the game!')
 
     def UIdata(self, redir):
         global name, userName, password
-        if redir == 'user_register':
+        if redir == 'u_reg':
             print('Registration:\n' + '-'*35 )
-            Register().reg_req(parsingInfo = 'file_getName')
+            Register().reg_req(redir = 'f-getN')
             name = input('Full Name: ').title()
-            Register().reg_req(parsingInfo = 'file_getUserName')
+            Register().reg_req(redir = 'f-getUN')
             userName = input('Username: ').lower()
-            Register().reg_req(parsingInfo = 'file_getPassword')
+            Register().reg_req(redir = 'f-getP')
             password = input('Password: ')
-        else:
+        else: 
             userName = input('Username: ').lower()
             password = input('Password: ')
 
-        if redir == 'user_register':
-            Validation().valid_reg(rfdbk, u_rfdbk)
-            Register().confirm_user(password)
-        elif redir == 'user_login':
-            Login().auth_data(userName, password, u_attempt)
+        if redir == 'u_reg': Validation().valid_reg(rfdbk, u_rfdbk), Register().confirm_user(password)
+        else: Login().auth_data(userName, password, u_attempt)
 
     def totline(self):
         global line
-        if os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK):
+        if os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK): 
             line = 0
-            with open('Auth/Inc/login.txt', 'r') as f:
-                for i in f:
-                    i
-                    line += 1
-        
-            
+            with open('Auth/Inc/login.txt', 'r') as f: 
+                for i in f: line += 1
+                i       
     def attempts(self, pathDecision):
-
         global u_attempt
-
         u_attempt += 1
-
-        if u_attempt == 3:
-            print("Attempts exceeded. Try again later.")
-            exit(time.sleep(3))
+        if u_attempt == 3: print("Attempts exceeded. Try again later."), exit(time.sleep(3))
         elif u_attempt == 1 and pathDecision == True:
             cRegAsk = input('We noticed that some information was not quite correct (details are mentioned above), do you want to continue with login or registration (Type either "L" OR "R")\nL/R: ').capitalize()
-        
-            if cRegAsk == 'L':
-                General().UIdata(redir = 'user_login')
-            elif cRegAsk == 'R':
-                General().UIdata(redir = 'user_register')
-            else:
-                General().attempts(pathDecision = True)
+            if cRegAsk == 'L': General().UIdata(redir = 'lgn')
+            elif cRegAsk == 'R': General().UIdata(redir = 'u_reg')
+            else: General().attempts(pathDecision = True)
 
 
 class Register:
-    # Handling gather & write user credentials to a LOCKED file | Collecting and checking if username used | validation userName, name and password
+
     def confirm_user(self, password):
         auth = False
         while auth == False:
             c_password = input('Confirm Password: ')
-
-            if password == c_password:
+            if password == c_password: 
                 Register().wFile_newUser(name, userName, password)
-                auth = True
-                os.system('cls' if os.name == 'nt' else "printf '\033c'")
-                exit(input("Success!\nYou must restart the program to continue to sign in.\nPress ENTER to EXIT."))
-                
-            else:
-                print("Passwords do not match. Try Again.")
-                General().attempts(pathDecision = False)
+                auth = True, os.system('cls' if os.name == 'nt' else "printf '\033c'"), exit(input("Success!\nYou must restart the program to continue to sign in.\nPress ENTER to EXIT."))
+            else: print("Passwords do not match. Try Again."), General().attempts(pathDecision = False)
 
     def wFile_newUser(self, name, userName, password):
         Validation().uRemove_def()
-        with open('Auth/Inc/login.txt', 'a') as contents:
-            contents.write(f'{name}|{userName}|{password}|\n')
-            contents.close()
+        with open('Auth/Inc/login.txt', 'a') as contents: contents.write(f'{name}|{userName}|{password}|\n'), contents.close()
 
-    def reg_req(self, parsingInfo):
-
+    def reg_req(self, redir):
         loc = 'Auth/Inc/paramRequirements.txt'
-        
-        if parsingInfo == 'file_getName':
-            print(l.getline(loc, 1))
-        elif parsingInfo == 'file_getUserName':
-            print(l.getline(loc, 2))
-        elif parsingInfo == 'file_getPassword':
-            print(l.getline(loc, 3))
-        
-        print("-"*50)
+        if redir == 'f-getN': print(l.getline(loc, 1))
+        elif redir == 'f-getUN': print(l.getline(loc, 2))
+        elif redir == 'f-getP': print(l.getline(loc, 3))
+        print(f"{'-'*50}")
+
 
 class Login:
-    # Handling show UI to user (UserName, Password) | looking for contents in locked file.
 
     def auth_data(self, userName, password, u_attempt):
         cc = False
         while cc == False:
             if line != c_line:
-                if userName == sys_userName and password == sys_password:
-                    Login().auth_con()
-                else:
-                    General().sys_data(r_action = '+ln')
+                if userName == sys_userName and password == sys_password: Login().auth_con()
+                else: General().sys_data(r_action = '+ln')
             else:
-                if userName == sys_userName and password == sys_password:
-                    Login().auth_con()
-                elif userName == sys_userName and password != sys_password:
-                    General().sys_data(r_action = '.ln')
-                    Validation().log_handle(rfdbk = 'xerr_pin')
-                elif userName != sys_userName and password == sys_password:
-                    General().sys_data(r_action = '.ln')
-                    Validation().log_handle(rfdbk = 'xerr_uin')
-                else:
-                    General().sys_data(r_action = '.ln')
-                    Validation().log_handle(rfdbk = 'xerr_pui_nfd')
+                if userName == sys_userName and password == sys_password: Login().auth_con()
+                elif userName == sys_userName and password != sys_password: General().sys_data(r_action = '.x'), Validation().log_handle(rfdbk = 'xerr_pin')
+                elif userName != sys_userName and password == sys_password: General().sys_data(r_action = '.x'), Validation().log_handle(rfdbk = 'xerr_uin')
+                else: General().sys_data(r_action = '.x') , Validation().log_handle(rfdbk = 'xerr_pui_nfd')
 
     def auth_con(self):
         global cc
-
         cc = True
         os.system('cls' if os.name == 'nt' else "printf '\033c'")
         print('Logged In!')
@@ -161,130 +108,80 @@ class Validation:
     def chk_data(self):
         General().totline()
         General().sys_data(r_action = 'reset_line')
-
         if sys_name == 'undefined' or sys_userName == 'undefined' or sys_password == 'undefined':
             print('Looks like you\'re not in the system.\nRedirecting you to registration now.')
             time.sleep(3), os.system('cls' if os.name == 'nt' else "printf '\033c'")
-            General().UIdata(redir = 'user_register')
+            General().UIdata(redir = 'u_reg')
 
     def uRemove_def(self):
-        General().sys_data(r_action = '.ln')
+        General().sys_data(r_action = '.x')
         General().totline()
-        if os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK):
-            if sys_name == 'undefined':
-                open('Auth/Inc/login.txt', 'w').close()
-
+        if (os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK)) and sys_name == 'undefined': open('Auth/Inc/login.txt', 'w').close()
 
     def log_handle(self, rfdbk):
         if rfdbk == 'xerr_pin':
             print("That password isn't linked to that account.")
             General().attempts(pathDecision = False)
-        elif rfdbk == 'xerr_uin':
-            print("That username was not found.")
+        elif rfdbk == 'xerr_uin': print("That username was not found.")
         elif rfdbk == 'xerr_pui_nfd':
             print("The username OR the password was not found.")
             General().attempts(pathDecision = True)
-    
-        if (3 - u_attempt) >= 1:
-            print("Try Again.")
-
+        
+        if (3 - u_attempt) >= 1: print("Try Again.")
         print("\nYou have " + str(3 - u_attempt) + " attempt(s) remaining.")
         print('-'*50)
-        General().UIdata(redir = 'user_login')
+        General().UIdata(redir = 'lgn')
         os.system('cls' if os.name == 'nt' else "printf '\033c'")
 
     def chk_exist_user(self, rfdbk, u_rfdbk):
         if os.path.isfile('Auth/Inc/login.txt') and os.access('Auth/Inc/login.txt', os.R_OK):
-            dataCheckComplete = False
-            rfdbk = 'un_exst'
-            firstCheck = 'ic'
-
-            while dataCheckComplete == False:
-
+            dcc, rfdbk, fc = False, 'un_exst', 'ic'
+            while dcc == False:
                 General().totline()
-                if firstCheck == 'ic':
-                    General().sys_data(r_action = '.ln')
-                elif firstCheck == 'c':
-                    General().sys_data(r_action = '+ln')
-
-                if line != c_line:
-                    if sys_userName == userName:
-                        u_rfdbk = 'u_occ'
-                        dataCheckComplete = True
-                    elif sys_userName != userName:
-                        rfdbk = 'u_acc'
-                        dataCheckComplete = False
-                    firstCheck = 'c'
-
-                else:
-                    if sys_userName != userName:
-                        rfdbk = 'u_acc'
-                    elif sys_userName == userName:
-                        u_rfdbk = 'u_occ'
-
-                    dataCheckComplete = True
+                if fc == 'ic':General().sys_data(r_action = '.x')
+                else: General().sys_data(r_action = '+ln')
+                if line != c_line and sys_userName == userName: u_rfdbk, dcc = 'u_occ', True
+                elif sys_userName != userName and line == c_line: rfdbk, dcc = 'u_acc', True
+                else: u_rfdbk, dcc = 'u_occ', True
+                fc = 'c'
             
             Validation().reg_handle(rfdbk, u_rfdbk)
 
     def valid_reg(self, rfdbk, u_rfdbk):
-        validate = False
-
-        while validate == False:
-            if len(name) <= 3 or name is any(x.isdigit() for x in name) or ' ' not in name:
-                Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'name')
-            elif len(userName) < 4 or ' ' in userName:
-                Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'userName')
-            elif (len(password) < 8 and len(password) > 60) or not any(x.isupper() for x in password) or not any(x.isdigit() for x in password):
-                Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'password')
-            else:
+        xdt = False
+        while xdt == False:
+            if len(name) <= 3 or name is any(x.isdigit() for x in name) or ' ' not in name: Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'name')
+            elif len(userName) < 4 or ' ' in userName: Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'userName')
+            elif (len(password) < 8 and len(password) > 60) or not any(x.isupper() for x in password) or not any(x.isdigit() for x in password): Validation().reg_handle(rfdbk = 'fmt-x', u_rfdbk = 'password')
+            else: 
                 Validation().chk_exist_user(rfdbk, u_rfdbk)
                 print('Formatting Validation Complete!\n')
-                validate = True
+                xdt = True
 
     def reg_handle(self, rfdbk, u_rfdbk):
         if rfdbk == 'fmt-x':
-            if u_rfdbk == 'name':
-                Register().reg_req(parsingInfo = 'file_getName')
-            elif u_rfdbk == 'userName':
-                Register().reg_req(parsingInfo = 'file_getUserName')
-            elif u_rfdbk == 'password':
-                Register().reg_req(parsingInfo = 'file_getPassword')
-            
+            if u_rfdbk == 'name': Register().reg_req(redir = 'f-getN')
+            elif u_rfdbk == 'userName': Register().reg_req(redir = 'f-getUN')
+            elif u_rfdbk == 'password': Register().reg_req(redir = 'f-getP')
             General().attempts(pathDecision = False)
-            if (4 - u_attempt) >= 1:
-                print("Try Again.")
-
-            print("\nYou have " + str(3 - u_attempt) + " attempt(s) remaining.")
-            print('-'*50)
+            print(f"\n{'Try Again.' if 4 - u_attempt >= 1 else ''}\nYou have {3 - u_attempt} attempt{'' if 3 - u_attempt == 1 else 's'} remaining.\n{'-'*50}\n")
             General().UIdata(redir = 'user_register')
 
-        elif rfdbk == 'un_exst':
-            if u_rfdbk == 'u_occ':
-                print('That userName already exists!')
-            
+        elif rfdbk == 'un_exst' and u_rfdbk == 'u_occ': 
+            print('That userName already exists!')
             General().attempts(pathDecision = False)
-            if (3 - u_attempt) >= 1:
-                print("Try Again.")
-
-            print("\nYou have " + str(3 - u_attempt) + " attempt(s) remaining.")
-            print('-'*50)
-            General().UIdata(redir = 'user_register')
+            print(f"\n{'Try Again.' if 4 - u_attempt >= 1 else ''}You have {3 - u_attempt} attempt{'' if 3 - u_attempt == 1 else 's'} remaining.\n{'-'*50}\n")
+            General().UIdata(redir = 'u_reg')
             os.system('cls' if os.name == 'nt' else "printf '\033c'")
              
         elif rfdbk == 'u_acc':
             os.system('cls' if os.name == 'nt' else "printf '\033c'")
 
 a = input("Hello User! This program requires authorisation to access. Register (R) or Login (L)?\nR/L: ").capitalize()
-
 if a == 'Reset':
     r = input('Reset data? (reset)\n').lower()
     if r == 'reset':
-        with open('Auth/Inc/Login.txt', 'w') as f:
-            f.write('undefined|'*3)
-            f.close()
+        with open('Auth/Inc/Login.txt', 'w') as f: f.write(f"{'undefined|'*3}"), f.close()
         exit('Complete.')
-elif a == 'R':
-    General().UIdata(redir = 'user_register')
-else:
-    Validation().chk_data()
-    General().UIdata(redir = 'user_login')
+elif a == 'R': General().UIdata(redir = 'u_reg')
+else: Validation().chk_data(), General().UIdata(redir = 'u_lgn')
